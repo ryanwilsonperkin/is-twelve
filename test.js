2f8eeb9a3910b97c05fef8eb8b4f4cc24bb45b09
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2016-03-23 19:09:17 -0400

    More test coverage.
    Gotta make sure this is airtight.

--- test.js
+++ test.js
@@ -5,3 +5,6 @@ function assert(condition) {
 }
 
 assert(isTwelve(12))          // 12 is 12
+assert(!isTwelve(4))          // 4 is not 12
+assert(!isTwelve("12"))       // "12" is not 12
+assert(!isTwelve("Twelve"))   // "Twelve" is not 12
